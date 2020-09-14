f9b6d407f6318e4baab3f90a01af5f184ed4b2af
Aaron <feldmanaaron406@gmail.com>
2020-09-14 17:58:42 -0300

    nuevos cambios

---
+++ main.js
@@ -0,0 +1,5 @@
+const validator = require ('validator');
+const chalk = require ('chalk');
+console.log(validator.isEmail('feldmanaaron406@gmail.com'));
+console.log(chalk.blue('hola mundo en azul'))
+
